7cc473f2c48b7a12f24e416d434dae46e5b60255
Renan Rodrigues <renan@MacBook-Pro-de-Thalia.local>
2021-05-02 17:59:44 -0300

    Comecando o projeto

---
+++ valida_login.php
@@ -0,0 +1,25 @@
+<?php
+    $usuario_autenticado = false;
+    $usuario_sistema = array(
+        array('email' => 'adm@teste.com.br', 'senha' => '123456'),
+        array('email' => 'user@teste.com.br', 'senha' => 'abc')
+    );
+    
+    foreach($usuario_sistema as $user)
+    {
+        
+        if(($user['email'] == $_POST['email']) && ($user['senha'] == $_POST['senha']))
+        {
+            $usuario_autenticado = true;
+            //header('Location: index.php?login=erro');
+        }
+
+        
+    }
+
+    if($usuario_autenticado){
+        echo "Usuario autenticado com sucesso";
+    }
+    else {
+        header('Location: index.php?login=erro');
+    }
