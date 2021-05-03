45a68a095baaa91f6d063433590c65c21f79c8d5
Renan Rodrigues <renan_jacui@hotmail.com>
2021-05-03 07:49:01 -0300

    Consultando chamdaos abertos

--- consultar_chamado.php
+++ consultar_chamado.php
@@ -1,5 +1,15 @@
 <?php
   require_once("validar_acesso.php");
+
+  $chamados = array();
+  //abri o arquivo
+  $arquivo = fopen('arquivo.hd', 'r');
+
+  //percorrer o arquivo
+  while(!feof($arquivo))
+  {
+    $chamados[] = fgets($arquivo);
+  }
 ?>
 <html>
   <head>
@@ -36,24 +46,23 @@
             </div>
             
             <div class="card-body">
-              
+              <?php
+                foreach ($chamados as $chamado) :
+
+                  $texto = explode('#', $chamado);
+                  if(count($texto) < 3){
+                    continue;
+                  }
+              ?>
               <div class="card mb-3 bg-light">
                 <div class="card-body">
-                  <h5 class="card-title">Título do chamado...</h5>
-                  <h6 class="card-subtitle mb-2 text-muted">Categoria</h6>
-                  <p class="card-text">Descrição do chamado...</p>
+                  <h5 class="card-title"><?=$texto[0]?></h5>
+                  <h6 class="card-subtitle mb-2 text-muted"><?=$texto[1]?></h6>
+                  <p class="card-text"><?=$texto[2]?></p>
 
                 </div>
               </div>
-
-              <div class="card mb-3 bg-light">
-                <div class="card-body">
-                  <h5 class="card-title">Título do chamado...</h5>
-                  <h6 class="card-subtitle mb-2 text-muted">Categoria</h6>
-                  <p class="card-text">Descrição do chamado...</p>
-
-                </div>
-              </div>
+              <?php endforeach?>
 
               <div class="row mt-5">
                 <div class="col-6">
